d3b78fa26c04323ca646b2eda87efdbb0be8291d
Senan555 <senanmurtuzayev003@gmail.com>
2021-10-22 01:13:45 +0400

    first commit

---
+++ game.js
@@ -0,0 +1,63 @@
+const list = ["paper.png","scissors.png","stone.png"]
+
+
+//DOM - Document Object Model
+
+const paper = document.getElementById('paper')
+const scissors = document.getElementById('scissors')
+const stone = document.getElementById('stone')
+
+
+
+const computerChoice = document.getElementById('computer-choice')
+
+const WriteResult = document.getElementById( 'mainresult')
+
+paper.onclick = MakeYourChoice
+scissors.onclick = MakeYourChoice
+stone.onclick = MakeYourChoice
+
+function MakeYourChoice() {
+    const computer = RandomChoice()
+    const choice = this.dataset.id
+
+
+if(computer == 0 && choice == 0){
+        result = "Try Again"
+    }
+
+else if(computer == 0 && choice == 1 ){
+        result = "Congratulations! You Won"
+}
+else if(computer == 0 && choice ==2 ){
+    result = "You Lost!"
+}
+else if(computer == 1 && choice ==0 ){
+    result = "You Lost!"
+}
+else if(computer == 1 && choice ==1 ){
+    result = "Try Again!"
+}
+else if(computer == 1 && choice ==2 ){
+    result = "Congratulations!You Won"
+}
+else if(computer == 2 && choice ==0 ){
+    result = "Congratulations!You won"
+}
+else if(computer == 2 && choice ==1 ){
+    result = "You Lost"
+}
+else if(computer == 2 && choice ==2 ){
+    result = "Try again"
+}
+
+
+WriteResult.innerHTML = result
+
+}
+
+function RandomChoice(){
+    const number = Math.round(Math.random()*2) // 0-2 arasindaki tam ededleri aliriq (0,1,2)
+    computerChoice.src =`img/${list[number]}` // template literal --- ${}
+    return number
+}

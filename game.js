10efa91326da1a8970ae452a7b61694eb9dad432
Senan555 <senanmurtuzayev003@gmail.com>
2021-10-24 03:18:44 +0400

    second commit

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 const list = ["paper.png","scissors.png","stone.png"]
 
+const list1 = [ "0-0.jpg","0-1.jpg","0-2.jpg","1-0.jpg","1-1.jpg","1-2.jpg","2-0.jpg","2-1.jpg","2-2.jpg"]
+
 
 //DOM - Document Object Model
 
@@ -8,6 +10,8 @@ const scissors = document.getElementById('scissors')
 const stone = document.getElementById('stone')
 
 
+
+const randomShekil = document.getElementById('random-shekil')
 
 const computerChoice = document.getElementById('computer-choice')
 
@@ -23,8 +27,9 @@ function MakeYourChoice() {
 
 
 if(computer == 0 && choice == 0){
-        result = "Try Again"
+    result = "Try again"
     }
+
 
 else if(computer == 0 && choice == 1 ){
         result = "Congratulations! You Won"
@@ -49,12 +54,16 @@ else if(computer == 2 && choice ==1 ){
 }
 else if(computer == 2 && choice ==2 ){
     result = "Try again"
+
 }
 
-
+randomShekil.src = `img/${list[computer]}`
 WriteResult.innerHTML = result
 
+
 }
+
+
 
 function RandomChoice(){
     const number = Math.round(Math.random()*2) // 0-2 arasindaki tam ededleri aliriq (0,1,2)
